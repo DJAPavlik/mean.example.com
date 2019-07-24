e030777f582624de97defcf3144784446bd3c0a0
Dennis Pavlik <DJAPavlik@gmail.com>
2019-07-24 15:23:26 -0500

    After initial testing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Super Express' , name : "Charlie Brown" });
 });
 
 module.exports = router;
