b5630cd678b35350f4cfecaee2a96b12e694242a
Dennis Pavlik <DJAPavlik@gmail.com>
2019-07-25 11:21:28 -0500

    Added CRUD for Users

--- app.js
+++ app.js
@@ -6,13 +6,20 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiUsersRouter = require('./routes/api/users');
 
 var app = express();
 
 var config = require('./config.dev');
 
+var mongoose = require('mongoose');
+
 //Test the file
 // console.log(config);
+
+//Connect to MongoDB
+mongoose.connect(config.mongodb, { useNewUrlParser: true });
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/users', apiUsersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
